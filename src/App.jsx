9124a659da8416d46022731c23c97b216685cf87
CHAPID611 <nechapidc22@itp.edu.co>
2025-03-02 12:40:53 -0500

    agregando pagina de productos y sus estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import {BrowserRouter as Router, Routes, Route}from "react-router-dom";
 import Home from "./pages/home";
+import Products from "./pages/products";
 
 const App = () => {
   return(
     <Router>
       {/* Definir las rutas de la aplicación */}
       <Routes>
-        <Route path ="/" element={<Home/>} />
+        <Route path ="/" element={<Home/>} />,
+        <Route path ="/productos" element={<Products/>} />
       </Routes>
     </Router>
   );
